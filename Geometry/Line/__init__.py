f295e6a0b910063264b9f505391c63a8a6d8544b
Qithron <80891835+qithron@users.noreply.github.com>
2021-09-11 09:31:53 +0700

    Delete Geometry directory

--- Geometry/Line/__init__.py
+++
@@ -1,6 +0,0 @@
-from .line import line # class
-from .ray import ray # class(line)
-from .segment import segment # class(ray)
-from .linear import linear
-from .perpendicular import perpendicular
-from .slope import slope
